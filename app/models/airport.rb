dae3af605a58af8cfd1d016a77e164c9974f4f88
Dennis Ojeda <royojeda1@gmail.com>
2022-07-10 21:33:42 +0800

    Add flight-airport associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,6 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights, foreign_key: :departure_airport_id,
+                               inverse_of: :departure_airport
+  has_many :arriving_flights, foreign_key: :arrival_airport_id,
+                              inverse_of: :arrival_airport
 end
