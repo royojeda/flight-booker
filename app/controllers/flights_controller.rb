b51249103b960d1696ad702b008e40f32d94868b
Dennis Ojeda <royojeda1@gmail.com>
2022-07-11 15:13:08 +0800

    Fix bug with FlightsController
    Handle cases where params[:flight] does not exist.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,9 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.where(flight_params)
-    @passenger_count = params[:passenger_count]
+    if params[:flight]
+      @flights = Flight.where(flight_params)
+      @passenger_count = params[:passenger_count]
+    end
     @airports = Airport.all.map { |a| [a.name, a.id] }
     @dates = Flight.all.map { |f| [f.departure_date, f.departure_date] }.uniq
   end
